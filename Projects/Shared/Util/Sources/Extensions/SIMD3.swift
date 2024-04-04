1edb22e938a57c876298fcc2e940a1e38eeec370
HJEHA <hyhpwang4@gmail.com>
2024-04-04 17:42:48 +0900

    [feat] Shared - Util 3차원 백터 거리 구하는 메서드 구현
    # <타입>: <제목>
    
    ##### 제목은 최대 50 글자까지만 입력 ############## -> |
    
    # 본문은 위에 작성
    ######## 본문은 한 줄에 최대 72 글자까지만 입력 ########################### -> |
    
    # 꼬릿말은 아래에 작성: ex) #이슈 번호
    
    # --- COMMIT END ---
    # <타입> 리스트
    #   feat    : 기능 (새로운 기능)
    #   fix     : 버그 (버그 수정)
    #   refactor: 리팩토링
    #   style   : 스타일 (코드 형식, 세미콜론 추가: 비즈니스 로직에 변경 없음)
    #   docs    : 문서 (문서 추가, 수정, 삭제)
    #   test    : 테스트 (테스트 코드 추가, 수정, 삭제: 비즈니스 로직에 변경 없음)
    #   chore   : 기타 변경사항 (빌드 스크립트 수정 등)
    # ------------------
    #     제목 첫 글자를 대문자로
    #     제목은 명령문으로
    #     제목 끝에 마침표(.) 금지
    #     제목과 본문을 한 줄 띄워 분리하기
    #     본문은 "어떻게" 보다 "무엇을", "왜"를 설명한다.
    #     본문에 여러줄의 메시지를 작성할 땐 "-"로 구분
    # ------------------

--- Projects/Shared/Util/Sources/Extensions/SIMD3.swift
+++ Projects/Shared/Util/Sources/Extensions/SIMD3.swift
@@ -5,4 +5,15 @@
 //  Created by 황제하 on 4/4/24.
 //
 
-import Foundation
+import simd
+
+public extension SIMD3<Float> {
+  func distance(to point: SIMD3<Float>) -> Float {
+    let deltaX = self.x - point.x
+    let deltaY = self.y - point.y
+    let deltaZ = self.z - point.z
+    let distance = sqrt(deltaX * deltaX + deltaY * deltaY + deltaZ * deltaZ)
+    
+    return distance
+  }
+}
